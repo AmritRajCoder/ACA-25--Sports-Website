efe8d9c8673d1bd32f42bea0732272459e0b9c05
Amrit Raj <73699121+AmritRajCoder@users.noreply.github.com>
2021-04-04 16:02:33 +0530

    Add files via upload

---
+++ Results/scriptIplResults.js
@@ -0,0 +1,10 @@
+  var firebaseConfig = {
+    apiKey: "AIzaSyDR-GyBHrH822T_ypubwIMVvR3rGIAJrxI",
+    authDomain: "ipl-results-58fe9.firebaseapp.com",
+    databaseURL: "https://ipl-results-58fe9-default-rtdb.firebaseio.com",
+    projectId: "ipl-results-58fe9",
+    storageBucket: "ipl-results-58fe9.appspot.com",
+    messagingSenderId: "910592836357",
+    appId: "1:910592836357:web:cf9bdd6bf98d996c17ed6c"
+  };
+  firebase.initializeApp(firebaseConfig);
